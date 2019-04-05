26d8b0977fead520af7a1d89093db99df601cc14
andreybozhkov <bozhkov.andrey@gmail.com>
2019-04-05 23:08:59 +0300

    Updated send reminders functionality part 2

--- src/components/reminders/ShipmentsReminders.js
+++ src/components/reminders/ShipmentsReminders.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import Shipment from '../shipments/Shipment';
-import sendReminder from './sendReminder';
+import ReminderShipmentsTable from './ReminderShipmentsTable';
+import ReactDOMServer from 'react-dom/server';
 
 export default class ShipmentsReminders extends Component {
     constructor(props) {
@@ -10,7 +10,32 @@ export default class ShipmentsReminders extends Component {
     }
 
     sendReminder() {
-        sendReminder(this.props.haulierData.shipments);
+        let body = document.getElementById("tableShipments");
+        console.log(body.toString());
+
+        /*fetch('https://graph.microsoft.com/v1.0/me/sendMail', {
+            method: 'POST',
+            headers: {
+                'Authorization': sessionStorage.getItem('access_token_graph'),
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                'message': {
+                    'subject': 'Missing CMRs Reminder',
+                    'body': {
+                        'contentType': 'HTML',
+                        'content': `Hello,<br/>Please see below list of missing documents:<br/>${body}`
+                    },
+                    'toRecipients': [
+                        {
+                            'emailAddress': {
+                                'address': 'abo@ntgcontinent.se'
+                            }
+                        }
+                    ]
+                }
+            })
+        }).then((res) => console.log(res)).catch((err) => console.log(err));*/
     }
 
     render() {
@@ -25,39 +50,7 @@ export default class ShipmentsReminders extends Component {
                         {sessionStorage.getItem('access_token_graph') &&
                             <button type="button" className="btn btn-primary" onClick={this.sendReminder}>Send Reminder</button>
                         }
-                        <table className="table table-hover table-responsive">
-                            <thead>
-                                <tr>
-                                    <th scope="col">Customer</th>
-                                    <th scope="col">Trailer</th>
-                                    <th scope="col">Loading Address</th>
-                                    <th scope="col">Unloading Address</th>
-                                    <th scope="col">Delivery Date</th>
-                                    <th scope="col">Project ID</th>
-                                    <th scope="col">Shipment ID</th>
-                                    <th scope="col">Invoice Nr</th>
-                                    <th scope="col">Invoice Amount</th>
-                                    <th scope="col">Invoice Currency</th>
-                                    <th scope="col">Project Resp.</th>
-                                    <th scope="col">Requested Date</th>
-                                    <th scope="col">Status</th>
-                                    <th scope="col">Documents Needed</th>
-                                    <th scope="col">Received Date</th>
-                                    <th scope="col">Notes</th>
-                                    <th scope="col">Reminder Date</th>
-                                    <th scope="col">Invoice Nr Missing CMR</th>
-                                    <th scope="col">Modify</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {
-                                    this.props.haulierData.shipments.sort( (a, b) => Date.parse(b._kmd.ect) - Date.parse(a._kmd.ect))
-                                                        .map((shipment) => 
-                                                        <Shipment key={shipment._id} {...shipment} />
-                                    )
-                                }
-                            </tbody>
-                        </table>
+                        <ReminderShipmentsTable haulierData={this.props.haulierData} />
                     </div>
                 }
             </div>
